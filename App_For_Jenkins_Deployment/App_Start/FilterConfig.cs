21a6b23ef102b0eda16591a7676cfa28612a3f92
Last Bench Coder <lastbenchcoder@gmail.com>
2020-09-19 19:38:13 +0530

    Initial commit for Jenkins deployment

---
+++ App_For_Jenkins_Deployment/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace App_For_Jenkins_Deployment
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ App_For_Jenkins_Deployment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="App_For_Jenkins_Deployment.MvcApplication" Language="C#" %>
